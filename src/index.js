ac489feb350570fbf29ac1c210ef6a1d481f2f70
subhash <subhashgo@gmail.com>
2018-12-11 10:13:49 +0530

    First commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema.graphql')
+
+
+  let projects = [{
+    id: 'project-0',
+    name: 'Sunrise Hospital',
+    location: 'Kochi, Kerala'
+  },
+  {id: 'project-1',
+    name: 'Holiday Inn',
+    location: 'Singapore'
+  }]
+
+let idCount = projects.length
+  const resolvers = {
+    Query : {
+      allProjects: () => projects
+    },
+    Project: {
+      id: (root) => root.id,
+      name: (root) => root.name,
+      location: (root) => root.location,
+    },
+    Mutation: {
+      createProject: (root,args) => {
+        const project = {
+          id: `project-${idCount++}`,
+          name: args.name,
+          location: args.location,
+        }
+        projects.push(project)
+        return project
+      }
+    },
+  }
+
+
+  const server = new ApolloServer({typeDefs, resolvers });
+
+  server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ src/schema.graphql
@@ -0,0 +1,20 @@
+const { gql } = require('apollo-server');
+
+const schema = gql `
+  type Query {
+    allProjects: [Project!]!
+    project(id: ID!): Project
+  }
+
+  type Mutation {
+    createProject(name: String!): Project!
+  }
+
+  type Project{
+    id: ID!
+    name: String!
+    location: String!
+  }
+`
+
+module.exports = schema
